6a6428946cea2777433df4aa95cc0cd4a6940f6e
Jure Pirc <cryptosi@users.noreply.github.com>
2023-10-26 07:37:15 +0200

    Update main.h
    Rever blocksize back we test on testnet first

--- src/main.h
+++ src/main.h
@@ -24,7 +24,7 @@ class CReserveKey;
 class CWallet;
 
 /** The maximum allowed size for a serialized block, in bytes (network rule) */
-static const unsigned int MAX_BLOCK_SIZE = 64'000'000;
+static const unsigned int MAX_BLOCK_SIZE = 5000000;
 /** The maximum size for mined blocks */
 static const unsigned int MAX_BLOCK_SIZE_GEN = MAX_BLOCK_SIZE/2;
 /** The maximum size for transactions we're willing to relay/mine **/
